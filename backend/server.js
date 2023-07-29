9eba82cdf714b0ef6bcee5ff9d2b567dd4a0da29
bartosz <bartosz@bartosz1999.local>
2023-07-29 14:24:33 +0200

    MongoDB conection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import companyRoutes from "./routes/companyRoutes.js";
 import invoicesRouters from "./routes/invoicesRouters.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 

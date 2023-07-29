919fc1383fefc60fffc4ed713e987631d33d3482
bartosz <bartosz@bartosz1999.local>
2023-07-29 13:14:59 +0200

    Added simple routes with console log for post-registerUser, post-logoutUser, get-getUser, put-updateUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,4 +4,28 @@ const authUser = async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 };
 
-export { authUser };
+// Register user | POST | Public
+
+const registerUser = async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+};
+
+// Logout user | POST | Public
+
+const logoutUser = async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+};
+
+// Get user profiles | GET | Private
+
+const getUser = async (req, res) => {
+  res.status(200).json({ message: "Get User" });
+};
+
+// Update user | PUT | Public
+
+const updateUser = async (req, res) => {
+  res.status(200).json({ message: "Update User" });
+};
+
+export { authUser, registerUser, logoutUser, getUser, updateUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,16 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
 const router = express.Router();
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUser).put(updateUser);
 
 export default router;

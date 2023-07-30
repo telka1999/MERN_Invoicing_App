580054ebda8ed96cc1eac1f52dc9266bcd7881ce
bartosz <bartosz@bartosz1999.local>
2023-07-30 14:08:35 +0200

    Added updating invoice

--- backend/controllers/invoicesController.js
+++ backend/controllers/invoicesController.js
@@ -43,7 +43,40 @@ const getInvoice = async (req, res) => {
 // Update Invoice | PUT | Private
 
 const updateInvoice = async (req, res) => {
-  res.status(200).json({ message: "Update Invoice" });
+  const {
+    invoiceId,
+    invoiceNr,
+    placeOfIssue,
+    dateOfIssue,
+    deadlinePayments,
+    paymentMethod,
+    saleDate,
+    accountNumber,
+    buyer,
+    seller,
+    items,
+  } = req.body;
+
+  const invoice = await Invoice.findById(invoiceId);
+
+  if (invoice) {
+    invoice.invoiceNr = invoiceNr || invoice.invoiceNr;
+    invoice.placeOfIssue = placeOfIssue || invoice.placeOfIssue;
+    invoice.dateOfIssue = dateOfIssue || invoice.dateOfIssue;
+    invoice.deadlinePayments = deadlinePayments || invoice.deadlinePayments;
+    invoice.paymentMethod = paymentMethod || invoice.paymentMethod;
+    invoice.saleDate = saleDate || invoice.saleDate;
+    invoice.accountNumber = accountNumber || invoice.accountNumber;
+    invoice.buyer = buyer || invoice.buyer;
+    invoice.seller = seller || invoice.seller;
+    invoice.items = items || invoice.items;
+
+    const updatedInvoice = await invoice.save();
+
+    res.status(200).json(updatedInvoice);
+  } else {
+    res.status(404).json({ message: "Invoice not found" });
+  }
 };
 
 // Delete Invoice | DELETE | Private

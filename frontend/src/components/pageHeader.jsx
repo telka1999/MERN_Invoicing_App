74caaa0e9bfcf9f8f630953abcd0ab981fc0d70d
bartosz <bartosz@bartosz1999.local>
2023-08-02 19:46:15 +0200

    Ui for invoices page, table with pagination

--- frontend/src/components/pageHeader.jsx
+++ frontend/src/components/pageHeader.jsx
@@ -1,9 +1,24 @@
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 
-export const PageHeader = ({ title }) => {
+export const PageHeader = ({ title, button }) => {
   return (
-    <Typography variant="h1" fontSize={36} fontWeight={400} marginTop={2}>
-      {title}
-    </Typography>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Typography variant="h1" fontSize={36} fontWeight={400} marginTop={2}>
+        {title}
+      </Typography>
+      {button && (
+        <Button variant="contained" startIcon={<AddOutlinedIcon />}>
+          Add
+        </Button>
+      )}
+    </div>
   );
 };
--- frontend/src/pages/Invoices.jsx
+++ frontend/src/pages/Invoices.jsx
@@ -1,3 +1,97 @@
+import { PageHeader } from "../components/pageHeader";
+import { Box } from "@mui/material";
+import * as React from "react";
+import { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
 export const Invoices = () => {
-  return <h1>Invoices</h1>;
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <div>
+      <PageHeader title="Invoices" button={true} />
+      <Box
+        sx={{
+          marginTop: 3,
+          border: "1px solid rgba(0, 0, 0, 0.12)",
+          borderRadius: "4px",
+        }}
+      >
+        <TableContainer sx={{ borderBottom: "1px solid rgba(0, 0, 0, 0.12)" }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Invoice Nr</TableCell>
+                <TableCell>Company</TableCell>
+                <TableCell>Date Of Issue</TableCell>
+                <TableCell>Deadline Payments</TableCell>
+                <TableCell>Payment Method</TableCell>
+                <TableCell>Sale Date</TableCell>
+                <TableCell>Total</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  hover
+                  key={row.name}
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                    cursor: "pointer",
+                  }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>{row.calories}</TableCell>
+                  <TableCell>{row.fat}</TableCell>
+                  <TableCell>{row.carbs}</TableCell>
+                  <TableCell>{row.protein}</TableCell>
+                  <TableCell>{row.protein}</TableCell>
+                  <TableCell>{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Box>
+    </div>
+  );
 };

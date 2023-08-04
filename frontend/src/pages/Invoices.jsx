514d89f2322f3e95f2203c25225c0d97ab43aee7
bartosz <bartosz@bartosz1999.local>
2023-08-04 18:18:22 +0200

    Added invoice data to table in invoices page

--- frontend/src/pages/Invoices.jsx
+++ frontend/src/pages/Invoices.jsx
@@ -1,7 +1,7 @@
 import { PageHeader } from "../components/pageHeader";
 import { Box } from "@mui/material";
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -10,21 +10,25 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TablePagination from "@mui/material/TablePagination";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 export const Invoices = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [invoices, setInvoices] = useState([]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -35,9 +39,26 @@ export const Invoices = () => {
     setPage(0);
   };
 
+  useEffect(() => {
+    const fetchInvoices = async () => {
+      const res = await fetch("/api/invoices", {
+        method: "GET",
+        redirect: "follow",
+      });
+      const data = await res.json();
+      setInvoices(data);
+    };
+    fetchInvoices();
+  }, []);
+
+  const dateReadable = (date) => {
+    const d = new Date(date);
+    return `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
+  };
+
   return (
     <div>
-      <PageHeader title="Invoices" button={true} link="/add-invoice"/>
+      <PageHeader title="Invoices" button={true} link="/add-invoice" />
       <Box
         sx={{
           marginTop: 3,
@@ -59,33 +80,42 @@ export const Invoices = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  hover
-                  key={row.name}
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                    cursor: "pointer",
-                  }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell>{row.calories}</TableCell>
-                  <TableCell>{row.fat}</TableCell>
-                  <TableCell>{row.carbs}</TableCell>
-                  <TableCell>{row.protein}</TableCell>
-                  <TableCell>{row.protein}</TableCell>
-                  <TableCell>{row.protein}</TableCell>
-                </TableRow>
-              ))}
+              {invoices.map((invoice) => {
+                const totalGrossValue = invoice.items.reduce((total, curr) => {
+                  const vatSum = (curr.vat / 100) * curr.price;
+                  const grossValue = curr.price + vatSum;
+                  return total + grossValue;
+                }, 0);
+                return (
+                  <TableRow
+                    hover
+                    key={invoice._id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      cursor: "pointer",
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {invoice.invoiceNr}
+                    </TableCell>
+                    <TableCell>{invoice.buyer.compnayName}</TableCell>
+                    <TableCell>{dateReadable(invoice.dateOfIssue)}</TableCell>
+                    <TableCell>
+                      {dateReadable(invoice.deadlinePayments)}
+                    </TableCell>
+                    <TableCell>{invoice.paymentMethod}</TableCell>
+                    <TableCell>{dateReadable(invoice.saleDate)}</TableCell>
+                    <TableCell>{totalGrossValue.toFixed(2)}</TableCell>
+                  </TableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+          count={invoices.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}

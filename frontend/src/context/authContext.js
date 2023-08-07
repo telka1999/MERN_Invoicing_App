0b6a6fcf8e1440d8c92bb760f135cba1db7ff5b8
bartosz <bartosz@bartosz1999.local>
2023-08-07 20:57:25 +0200

    Edited vercel.json

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -18,7 +18,6 @@ function useProvideAuth() {
 
   useEffect(() => {
     const fetchUser = async () => {
-      console.log(process.env.REACT_APP_URL);
       try {
         const res = await fetch(
           `${process.env.REACT_APP_URL}/api/users/profile`,
@@ -27,9 +26,7 @@ function useProvideAuth() {
             redirect: "follow",
           }
         );
-        console.log(res);
         const data = await res.json();
-        console.log(data);
         if (data?.message) {
           setUser(null);
           setLoading(false);

1941b0b7a8079fdc9030c7d10d390c3047a7a0d9
bartosz <bartosz@bartosz1999.local>
2023-08-08 16:37:59 +0200

    Delted cors and added secure option to generate token

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
@@ -13,7 +12,6 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

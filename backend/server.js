704799873da3891f358aeace9d0032c59787cfae
bartosz <bartosz@bartosz1999.local>
2023-08-08 07:55:58 +0200

    Changed path in serverjs

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ app.get("/", (req, res) => res.send("Server is runing"));
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {

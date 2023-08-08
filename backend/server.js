a74698b9c680c976f29b664b47f8794eafb42f3d
bartosz <bartosz@bartosz1999.local>
2023-08-08 07:35:51 +0200

    Trying to deploye app to one url

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,19 @@ app.use("/api/invoices", invoicesRouters);
 
 app.get("/", (req, res) => res.send("Server is runing"));
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
